acb48def3ae50ffb820f0d8c6efa7e26c1b778a6
DiegoEmilio01 <diegobustama@uc.cl>
2020-04-22 21:22:21 -0400

    Primera piedra.

--- app/app/controllers/presentacion_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PresentacionController < ApplicationController
-  def informacion
-    @grupo = ['Diego Bustamante', 'Lucas Muñoz', 'Elías Sabja']
-    render 'informacion'
-  end
-end
--- app/app/controllers/saludo_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class SaludoController < ApplicationController
-  def hello
-    render 'hello'
-  end
-end
