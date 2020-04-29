0670491279ebaadd26a8e74036eb5f8cf496a046
unknown <elias.sabja@uc.cl>
2020-04-28 22:11:46 -0400

    CRUD comentarios

---
+++ app/controllers/comentarios_controller.rb
@@ -0,0 +1,47 @@
+class ComentariosController < ApplicationController
+
+  def new
+    @comentario = Comentario.new
+  end
+
+  def create
+    comentario_params = params.require(:comentario).permit(:contenido, :fecha)
+    @comentario = Comentario.create(comentario_params)
+
+    if @comentario.save
+      redirect_to comentarios_new_path, notice: "Comentario publicado"
+    else
+      redirect_to comentarios_new_path, notice: "Error al publicar comentario"
+    end
+  end
+
+  def index
+    @comentarios = Comentario.all
+  end
+
+  def show
+    @comentario = Comentario.find(params[:id])
+  end
+
+  def edit
+    @comentario = Comentario.find(params[:id])
+  end
+
+  def update
+    comentario_params = params.require(:comentario).permit(:contenido, :fecha)
+    @comentario = Comentario.find(params[:id])
+
+    if @comentario.update(comentario_params)
+      redirect_to comentario_path(@comentario.id), notice: "Comentario editado"
+    else
+      redirect_to comentario_path(@comentario.id), notice: "Hubo un error al editar el comentario"
+    end
+  end 
+
+  def destroy
+    @comentario = Comentario.find(params[:id])
+    @comentario.destroy
+    redirect_to comentarios_path, notice: 'Comentario eliminado definitivamente'
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_235152) do
+ActiveRecord::Schema.define(version: 2020_04_28_223540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comentarios", force: :cascade do |t|
+    t.string "contenido"
+    t.string "fecha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comunas", force: :cascade do |t|
     t.string "nombre"
