c28b34d93ed6c86fb0d94321be8bc170b43c4962
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-19 11:52:24 -0400

    Subir Owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,6 @@ class Owner < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nombre, presence: { message: "vacio" }
+  validates :nombre, uniqueness: { message: "existe" }
 end
