e6f43e249fd6f6b8677d5b7ce95fccb8a04c67a5
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-18 23:11:20 -0400

    D

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nombre, presence: { message: "Ingresó un Nombre en blanco." }
 end
