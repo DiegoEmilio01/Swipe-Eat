27e7f41f247d928ec8d0f4fbc1d83703c491db07
DiegoEmilio01 <diegobustama@uc.cl>
2020-05-18 22:38:05 -0400

    cruds sin device terminados.

--- app/models/gusto.rb
+++ app/models/gusto.rb
@@ -2,5 +2,5 @@ class Gusto < ApplicationRecord
     validates :nombre, presence: { message: "Ingresó un Nombre en blanco." }
     validates :nombre, uniqueness: { message: "Ya existe un gusto con ese Nombre." }
     validates :descripcion, presence: { message: "Ingresó una Descripción en blanco." }
-    validates :descripcion, length: { within: 10..100, message: "La descripción debe tener un largo entre 10 a 100 carácteres inclusive." }
+    validates :descripcion, length: { within: 10..100, message: "La Descripción debe tener un largo entre 10 a 100 carácteres inclusive." }
 end
--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,3 +1,7 @@
 class Restaurante < ApplicationRecord
     has_many_attached :imagenes
+    validates :nombre, presence: { message: "Ingresó un Nombre en blanco." }
+    validates :nombre, uniqueness: { message: "Ya existe un restaurante con ese Nombre." }
+    validates :descripcion, presence: { message: "Ingresó una Descripción en blanco." }
+    validates :descripcion, length: { within: 10..100, message: "La Descripción debe tener un largo entre 10 a 100 carácteres inclusive." }
 end
