808ace0124eb4a07658b434537b792f8254bdd99
EliasSabja <elias.sabja@uc.cl>
2020-06-04 16:36:43 -0400

    Ahora sign in como admin lleva a admin_root correctamente

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@
 
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
-    home_root_path
+    if _resource.class == Swiper
+      home_root_path
+    elsif _resource.class == Admin
+      admin_root_path
+    end
   end
 end
