92818fc9c25e9a4c618f9e46ed61b92b3be03220
EliasSabja <elias.sabja@uc.cl>
2020-06-04 16:38:11 -0400

    Debug de rubocop sobre la ruta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(_resource)
-    if _resource.class == Swiper
+  def after_sign_in_path_for(resource)
+    if resource.class == Swiper
       home_root_path
-    elsif _resource.class == Admin
+    elsif resource.class == Admin
       admin_root_path
     end
   end
