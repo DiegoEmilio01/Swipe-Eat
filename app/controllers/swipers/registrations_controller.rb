4691bd639e6d75fe6da50011ab91f68b63acccd5
EliasSabja <elias.sabja@uc.cl>
2020-06-05 16:25:04 -0400

    Error handle de registrations sin gustos

--- app/controllers/swipers/registrations_controller.rb
+++ app/controllers/swipers/registrations_controller.rb
@@ -72,10 +72,11 @@ class Swipers::RegistrationsController < Devise::RegistrationsController
     gustos = sign_up_params[:gusto_ids]
     sign_up_params.delete(:gusto_ids)
     build_resource(sign_up_params)
-
-    gustos.each do |gusto_id|
-      gusto = Gusto.find(gusto_id)
-      @swiper.gustos << gusto unless @swiper.gustos.include?(gusto)
+    if (gustos)
+      gustos.each do |gusto_id|
+        gusto = Gusto.find(gusto_id)
+        @swiper.gustos << gusto unless @swiper.gustos.include?(gusto)
+      end
     end
     resource.save
     yield resource if block_given?
