818b1d2bc535bcd59a98cbe781871f156662a970
EliasSabja <elias.sabja@uc.cl>
2020-06-21 17:29:09 -0400

    Validacion de fecha cita

--- app/controllers/meet_controller.rb
+++ app/controllers/meet_controller.rb
@@ -7,12 +7,12 @@ class MeetController < ApplicationController
     meet_params['estado'] = false
     @meet = Meet.create meet_params
     @swiper_id = params[:swiper_cita_id]
-    if @meet.save
+    if @meet.save && !params[:fecha] == ''
       @swiper_citado = Swiper.find params[:swiper_citado_id]
       redirect_to lista_citas_path(@swiper_id), notice: 'Cita agendada'
     else
       redirect_to fecha_cita_path(params[:swiper_cita_id], params[:swiper_cita_id],
-                                  params[:restaurante_id], 1), notice: @meet.errors
+                                  params[:restaurante_id], 1), notice: 'Fecha no válida'
     end
   end
 
